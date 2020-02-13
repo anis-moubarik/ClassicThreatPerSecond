bb3fc679acad7c6a3e3228c50c1f95d57f43d1b1
Anis Moubarik <anis.moubarik@gmail.com>
2020-02-13 22:44:46 +0200

    Update threat.js

--- threat.js
+++ threat.js
@@ -188,7 +188,7 @@ class Encounter {
                 this.breakdown[event_name] = (this.breakdown[event_name]||0)+t;
             }
 
-            console.log(this.threat, t, event);
+            //console.log(this.threat, t, event);
             this.threat += t;
         }		
     }
